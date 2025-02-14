c982ca558fe6f5f15f8f6e25c4027439d0dabc18
张斌 <bin.zhang@appshahe.com>
2025-02-14 14:10:26 +0800

    feat：C#调用lua函数（映射到LuaFunction）

--- Assets/Scripts/Lua2CSharp/CSharpCallFunction.cs
+++ Assets/Scripts/Lua2CSharp/CSharpCallFunction.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using XLua;
 
@@ -15,6 +16,8 @@ public class CSharpCallFunction : MonoBehaviour
         XLuaEnv.Instance.DoString("Lua2CSharp/CSharpCallFunction");
         
         MapDelegate();
+        
+        MapLuaFunction();
     }
     
     // 映射到Delegate
@@ -31,12 +34,33 @@ public class CSharpCallFunction : MonoBehaviour
         Func3 func3 = global.Get<string, Func3>("func3");
         Debug.Log(func3() + ", 被C#调用!");
         
-        Func4 func4 = global.Get<Func4>("func4");
+        Func4 func4 = global.Get<string, Func4>("func4");
         string name;
         int age = 18;
         func4(out name, ref age);
         
         Debug.Log($"name: {name}, age: {age}");
+    }
+
+    // 映射到LuaFunction
+    private void MapLuaFunction()
+    {
+        LuaTable global = XLuaEnv.Instance.Global;
+        
+        LuaFunction func1 = global.Get<string, LuaFunction>("func1");
+        func1.Call();
+        
+        LuaFunction func2 = global.Get<string, LuaFunction>("func2");
+        func2.Call("admin");
+        
+        LuaFunction func3 = global.Get<string ,LuaFunction>("func3");
+        object[] fun3Return = func3.Call();
+        Debug.Log((string)fun3Return[0] + ", 被C#调用!");
+        
+        LuaFunction func4 = global.Get<string ,LuaFunction>("func4");
+        object[] fun4Return = func4.Call();
+        Debug.Log($"name: {(string)fun4Return[0]}, age: {(Int64)fun4Return[1]}");
+        
     }
 
     private void Update()
